da0f7ab6ae2dd1aa63cb62a55f02e9fa7ad5c0da
kapilrc <kapilrc@gmail.com>
2019-11-01 17:30:54 +0530

    Set up React, concurrently, cleanup, navbar & Router setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className='my-1'>
+        This is a Full stack React app for Managing contacts
+      </p>
+      <div className='bg-dark p'>Version: 1.0.0</div>
+    </div>
+  );
+};
+
+export default About;
